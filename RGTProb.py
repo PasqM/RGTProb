5a1b3ff9acf62a54bc8437893d33168faf41bc2c
GuidoNarduzzi <guido.narduzzi@sns.it>
2019-03-28 18:39:19 +0100

    Update RGTProb.py

--- RGTProb.py
+++ RGTProb.py
@@ -1,5 +1,5 @@
 # Project developed by Guido Narduzzi and Pasquale Miglionico, undergraduate fellows of the Scuola Normale Superiore of Pisa and students of 
-# the Department of Biology of the University of Pisa, under the direction of Dr. Filippo Di Santo, University of Pisa, Department of Mathematics
+# the Department of Biology of the University of Pisa, under the direction of Dr. Filippo Disanto, University of Pisa, Department of Mathematics
 
 print('Welcome to RGTProb, the gene tree probability calculator. This tool will allow you to calculate Ranked Gene Tree probabilities conditioning on a species tree in multiple formats. You can modify the program as you like, but please always mention the original project. If you develop something interesting please let us know, so that we can add it to our software and distribute your content to other users.')
 
